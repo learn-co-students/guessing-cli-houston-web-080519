a2c76c215fe36e1884fc7c81866c09deb50696f6
Barrette Banner <barrettebanner@gmail.com>
2019-08-05 13:06:48 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,12 @@
 # Code your solution here!
+def run_guessing_game
+  number = rand(6) + 1 
+  input = gets.chomp 
+  if input == "exit"
+    puts "Goodbye!"
+  elsif input == number.to_s 
+    puts "You guessed the correct number!"
+  elsif input != number.to_s
+    puts "Sorry! The computer guessed #{number}."
+  end
+end
